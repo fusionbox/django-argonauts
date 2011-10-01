16443165ec4ae3065b75a64d08f6b4da3ca15041
Gavin Wahl <gwahl@fusionbox.com>
2011-10-01 12:54:34 -0600

    change add_fields_to_class to modify_schema, and let inheritance take care of calling it for every behavior

--- fusionbox/behaviors.py
+++ fusionbox/behaviors.py
@@ -7,12 +7,9 @@ class MetaBehavior(ModelBase):
     '''
     def __new__(cls, name, bases, attrs):
         new_class = super(MetaBehavior, cls).__new__(cls, name, bases, attrs)
-        print new_class.__name__, new_class._meta.abstract
-        if not new_class._meta.abstract or False:
+        if not new_class._meta.abstract:
             new_class.merge_parent_settings()
-            for behavior in new_class.base_behaviors():
-                behavior.add_fields_to_child(new_class)
-
+            new_class.modify_schema()
         return new_class
 
 class Behavior(models.Model):
@@ -53,6 +50,13 @@ class Behavior(models.Model):
     __metaclass__ = MetaBehavior
 
     @classmethod
+    def modify_schema(cls):
+        """
+        Hook for behaviors to modify their model class just after it's created
+        """
+        pass
+
+    @classmethod
     def merge_parent_settings(cls):
         behaviors = [behavior.__name__ for behavior in cls.base_behaviors()]
         for parent in reversed(cls.mro()):
@@ -75,9 +79,10 @@ class Behavior(models.Model):
                 behaviors.append(parent)
         return behaviors
 
+
 class TimeStampable(Behavior):
     '''
-    Base class for adding timestamping behavior to a model.  
+    Base class for adding timestamping behavior to a model.
 
     Added Fields:
         Field 1:
@@ -88,7 +93,7 @@ class TimeStampable(Behavior):
             field: DateTimeField(auto_now_add=True)
             description: Timestamps set each time the save method is called on the instance
             default_name: updated_at
-    
+
     '''
     class Meta:
         abstract = True
@@ -98,13 +103,15 @@ class TimeStampable(Behavior):
         updated_at_field_name = 'updated_at'
 
     @classmethod
-    def add_fields_to_child(cls, child):
-        child.add_to_class(child.TimeStampable.created_at_field_name, models.DateTimeField(auto_now_add=True))
-        child.add_to_class(child.TimeStampable.updated_at_field_name, models.DateTimeField(auto_now=True))
+    def modify_schema(cls):
+        cls.add_to_class(cls.TimeStampable.created_at_field_name, models.DateTimeField(auto_now_add=True))
+        cls.add_to_class(cls.TimeStampable.updated_at_field_name, models.DateTimeField(auto_now=True))
+        super(TimeStampable, cls).modify_schema()
+
 
 class SEO(Behavior):
     '''
-    Base class for adding seo behavior to a model.  
+    Base class for adding seo behavior to a model.
 
     Added Fields:
         Field 1:
@@ -121,7 +128,7 @@ class SEO(Behavior):
             description: Text field intended for use in html <meta name="keywords"> tag.
             validation: comma separated text strings
             default_name: seo_keywords
-    
+
     '''
     class Meta:
         abstract = True
@@ -132,7 +139,8 @@ class SEO(Behavior):
         seo_keywords_field_name = 'seo_keywords'
 
     @classmethod
-    def add_fields_to_child(cls, child):
-        child.add_to_class(child.SEO.seo_title_field_name, models.CharField(max_length = 255))
-        child.add_to_class(child.SEO.seo_description_field_name, models.TextField())
-        child.add_to_class(child.SEO.seo_keywords_field_name, models.TextField())
+    def modify_schema(cls):
+        cls.add_to_class(cls.SEO.seo_title_field_name, models.CharField(max_length = 255))
+        cls.add_to_class(cls.SEO.seo_description_field_name, models.TextField())
+        cls.add_to_class(cls.SEO.seo_keywords_field_name, models.TextField())
+        super(SEO, cls).modify_schema()
