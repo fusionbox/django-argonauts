28247d88e9212232fd24ba190e868188f0abd12a
Gavin Wahl <gwahl@fusionbox.com>
2011-10-04 13:43:39 -0600

    Start some bare bones documentation in sphinx

---
+++ doc/middleware.rst
@@ -0,0 +1,8 @@
+The Middlewares
+===============
+
+:class:`GenericTemplateFinderMiddleware` is useful during development to avoid
+having to write urlpatterns for every template.
+
+.. automodule:: fusionbox.middleware
+    :members:
--- fusionbox/behaviors.py
+++ fusionbox/behaviors.py
@@ -25,10 +25,10 @@ class MetaBehavior(ModelBase):
                 class FooBehavior:
                     some_column = 'another_name'
 
-        will be able to change the name of some_column to another_name.
+        will be able to change the name of ``some_column`` to ``another_name``.
 
-        To do this, we rip out all instances of model.Field, and wait for
-        Behavior.modify_schema to add them back in once all config classes are
+        To do this, we rip out all instances of :class:`model.Field`, and wait for
+        :func:`Behavior.modify_schema` to add them back in once all config classes are
         merged.
         """
 
@@ -62,29 +62,32 @@ class Behavior(models.Model):
     multi-inheritance as well.  Each behavior adds a set of default fields
     and/or methods to the model.  Field names can be customized like example B.
 
-    EXAMPLE A
-    class MyModel(FooBehavior):
-        pass
+    EXAMPLE A::
 
-    MyModel will have whatever fields FooBehavior adds with default field
-    names.
+        class MyModel(FooBehavior):
+            pass
 
-    EXAMPLE B
-    class MyModel(FooBehavior):
-        class FooBehavior:
-            bar = "qux"
-            baz = "quux"
+    ``MyModel`` will have whatever fields ``FooBehavior`` adds with default
+    field names.
 
-    MyModel will have the fields from FooBehavior added, but the field names
-    will be "qux" and "quux" respectively.
+    EXAMPLE B::
 
-    EXAMPLE C
-    class MyModel(FooBehavior, BarBehavior):
-        pass
+        class MyModel(FooBehavior):
+            class FooBehavior:
+                bar = "qux"
+                baz = "quux"
 
-    MyModel will have the fields from both FooBehavior and BarBehavior, each
-    with default field names.  To customizing field names can be done just like
-    it was in example B.
+    ``MyModel`` will have the fields from ``FooBehavior`` added, but the field
+    names will be "qux" and "quux" respectively.
+
+    EXAMPLE C::
+
+        class MyModel(FooBehavior, BarBehavior):
+            pass
+
+    ``MyModel`` will have the fields from both ``FooBehavior`` and
+    ``BarBehavior``, each with default field names.  To customizing field names
+    can be done just like it was in example B.
 
     """
     class Meta:
