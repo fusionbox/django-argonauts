328111c01c3c1fe98ad12ed0947a8b4fd44476e9
Gavin Wahl <gwahl@fusionbox.com>
2011-11-07 11:55:10 -0700

    postgres backup

--- fusionbox/management/commands/backupdb.py
+++ fusionbox/management/commands/backupdb.py
@@ -2,7 +2,7 @@
 Adapted from http://djangosnippets.org/snippets/823/
 """
 
-import os, time, pipes
+import os, time, pipes, popen2
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
@@ -17,15 +17,24 @@ class Command(BaseCommand):
 
         for database_name in settings.DATABASES:
             config = settings.DATABASES[database_name]
-            if config['ENGINE'] != 'django.db.backends.mysql':
-                print 'Backup in %s engine not implemented' % self.engine
             outfile = os.path.join(backup_dir, '%s-%s.sql.gz' % (database_name, time.strftime('%F-%s')))
-            self.do_mysql_backup(outfile,
-                                 config['NAME'],
-                                 config['USER'],
-                                 config.get('PASSWORD', None),
-                                 config.get('HOST', None),
-                                 config.get('PORT', None))
+            if config['ENGINE'] == 'django.db.backends.mysql':
+                self.do_mysql_backup(outfile,
+                                     config['NAME'],
+                                     config['USER'],
+                                     config.get('PASSWORD', None),
+                                     config.get('HOST', None),
+                                     config.get('PORT', None))
+            elif config['ENGINE'] == 'django.db.backends.postgresql_psycopg2':
+                self.do_postgresql_backup(outfile,
+                                     config['NAME'],
+                                     config['USER'],
+                                     config.get('PASSWORD', None),
+                                     config.get('HOST', None),
+                                     config.get('PORT', None))
+            else:
+                print 'Backup in %s engine not implemented' % config['ENGINE']
+
 
     def do_mysql_backup(self, outfile, db, user, password=None, host=None, port=None):
         args = []
@@ -42,4 +51,25 @@ class Command(BaseCommand):
         c = 'mysqldump %s | gzip > %s' % (' '.join(args), outfile)
         print c
         os.system(c)
-        print "Backed up %s; Load with `cat %s | gunzip > mysql %s`" % (db, pipes.quote(outfile), ' '.join(args))
+        print "Backed up %s; Load with `cat %s | gunzip | mysql %s`" % (db, pipes.quote(outfile), ' '.join(args))
+
+
+    def do_postgresql_backup(self, outfile, db, user, password=None, host=None, port=None):
+        args = []
+        if user:
+            args += ["--username=%s" % user]
+        if password:
+            args += ["--password"]
+        if host:
+            args += ["--host=%s" % host]
+        if port:
+            args += ["--port=%s" % port]
+        if db:
+            args += [db]
+        c = 'pg_dump %s > %s' % (' '.join(args), outfile)
+        print c
+        pipe = popen2.Popen4(c)
+        if password:
+            pipe.tochild.write('%s\n' % password)
+            pipe.tochild.close()
+        print "Backed up %s; Load with `cat %s | gunzip | psql %s`" % (db, pipes.quote(outfile), ' '.join(args))
