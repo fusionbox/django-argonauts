ff96f098cdb7dbe9e41fc9062354f6115dca5196
Aaron Merriam <amerriam@fusionbox.com>
2011-12-07 18:28:35 -0700

    fixes for finding template directory

---
+++ fusionbox/panels/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['user_panel',]
--- fusionbox/panels/user_panel/urls.py
+++ fusionbox/panels/user_panel/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, url
 
 from debug_toolbar.urls import _PREFIX
 
-urlpatterns = patterns('debug_toolbar_user_panel.views',
+urlpatterns = patterns('fusionbox.panels.user_panel.views',
     url(r'^%s/users/$' % _PREFIX, 'content',
         name='debug-userpanel'),
     url(r'^%s/users/login/$' % _PREFIX, 'login',
