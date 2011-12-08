5a1216e920aa4d17ee435938f18c285647a680ad
Aaron Merriam <amerriam@fusionbox.com>
2011-12-07 17:00:21 -0700

    Implemented basic architecture changes
    Changed various things in how the panel was designed to closer fit
    personal design and architecture choices.
    
    *  Removed form view which felt unnecessarily complex
    *  Changed how current user attribute dictionary is compiled
    *  Switched views to `env` dictionary architecture
    *  Changed render functions in views
    *  Other minor tweaks

--- fusionbox/panels/user_panel/models.py
+++ fusionbox/panels/user_panel/models.py
@@ -2,7 +2,7 @@ import debug_toolbar.urls
 
 from django.conf.urls.defaults import patterns, include
 
-from .urls import urlpatterns
+from fusionbox.panels.user_panel.urls import urlpatterns
 
 debug_toolbar.urls.urlpatterns += patterns('',
     ('', include(urlpatterns)),
--- fusionbox/panels/user_panel/panels.py
+++ fusionbox/panels/user_panel/panels.py
@@ -1,64 +1,3 @@
-"""
-:mod:`django-debug-toolbar-user-panel`
-======================================
-
-Panel for the `Django Debug Toolbar <https://github.com/django-debug-toolbar/django-debug-toolbar>`_
-to easily and quickly switch between users.
-
- * View details on the currently logged in user.
- * Login as any user from an arbitrary email address, username or user ID.
- * Easily switch between recently logged in users.
-
-.. figure::  screenshot.png
-   :align:   center
-
-The panel supports ``django.contrib.auth.models.User`` models that have had
-the `username` field removed.
-
-Installation
-------------
-
-Add ``debug_toolbar_user_panel`` to your ``INSTALLED_APPS``::
-
-    INSTALLED_APPS = (
-        ...
-        'debug_toolbar_user_panel',
-        ...
-    )
-
-Add ``debug_toolbar_user_panel.panels.UserPanel`` to ``DEBUG_TOOLBAR_PANELS``::
-
-    DEBUG_TOOLBAR_PANELS = (
-        'debug_toolbar_user_panel.panels.UserPanel'
-        'debug_toolbar.panels.version.VersionDebugPanel',
-        'debug_toolbar.panels.timer.TimerDebugPanel',
-        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-        'debug_toolbar.panels.headers.HeaderDebugPanel',
-        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-        'debug_toolbar.panels.sql.SQLDebugPanel',
-        'debug_toolbar.panels.template.TemplateDebugPanel',
-        'debug_toolbar.panels.signals.SignalDebugPanel',
-        'debug_toolbar.panels.logger.LoggingPanel',
-    )
-
-Include ``debug_toolbar_user_panel.urls`` somewhere in your ``urls.py``::
-
-    urlpatterns = patterns('',
-        ...
-        url(r'', include('debug_toolbar_user_panel.urls')),
-        ...
-    )
-
-Links
------
-
-View/download code
-  https://github.com/playfire/django-debug-toolbar-user-panel
-
-File a bug
-  https://github.com/playfire/django-debug-toolbar-user-panel/issues
-"""
-
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
@@ -91,7 +30,7 @@ class UserPanel(DebugPanel):
             'request': self.request,
         })
 
-        return render_to_string('debug_toolbar_user_panel/panel.html', context)
+        return render_to_string('fusionbox/panels/user_panel/panel.html', context)
 
     def process_request(self, request):
         force_secure = not getattr(settings, 'DEBUG_TOOLBAR_USER_PANEL_SECURE', False)
