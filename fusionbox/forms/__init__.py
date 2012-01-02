2c457d2a758e3bedf46ac01d69b5ee04ba9145f4
Gavin Wahl <gwahl@fusionbox.com>
2012-01-02 16:22:21 -0700

    MonthField and FutureYearField select month/years

--- fusionbox/forms/__init__.py
+++ fusionbox/forms/__init__.py
@@ -1,3 +1,26 @@
 from __future__ import absolute_import
 
+import calendar
+import datetime
+
+from django import forms
+
 from fusionbox.forms.models import *
+
+
+class MonthField(forms.TypedChoiceField):
+    def __init__(self, *args, **kwargs):
+        super(MonthField, self).__init__(*args, **kwargs)
+        self.choices = [('', ' -- ')] + \
+                [(i, "%s - %s" % (i, calendar.month_name[i]))
+                        for i in range(1, 13)]
+        self.coerce = int
+
+class FutureYearField(forms.TypedChoiceField):
+    def __init__(self, *args, **kwargs):
+        number_of_years = kwargs.pop('number_of_years', 6)
+        super(FutureYearField, self).__init__(*args, **kwargs)
+        self.choices = [('', ' -- ')] + \
+                [(i%100, str(i))
+                        for i in range(datetime.datetime.now().year, datetime.datetime.now().year + number_of_years)]
+        self.coerce = int
