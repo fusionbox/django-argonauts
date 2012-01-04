60d240ca251848864ec49e6ac262d7ae72c4cae7
Chris Proto <cproto@fusionbox.com>
2012-01-04 12:40:40 -0700

    add option to output javascript somewhere else on the page

--- fusionbox/newsletter/templatetags/newsletter.py
+++ fusionbox/newsletter/templatetags/newsletter.py
@@ -1,12 +1,14 @@
 """
 The newsletter template tag library
 
-To load the tag in a Django template:
-
 `{% load newsletter %}`
 
-"""
+To use the newsletter template tags.  It is required
+to have jquery.forms.js installed.
 
+This is included with the installation under the `static` directory.
+
+"""
 from django import template
 from django.core.urlresolvers import reverse
 
@@ -14,14 +16,18 @@ register = template.Library()
 
 class NewsletterNode(template.Node):
 
-    def render(self, context):
-        return (
-"""
-<div id="newsletter_container" class="newsletter"></div>
+    def __init__(self, render_html, render_js):
+        self.render_js = render_js
+        self.render_html = render_html
 
-<script type="text/javascript">
+    def render(self, context):
+        html = (self.render_html and
+                '<div id="newsletter_container" class="newsletter"></div>'
+                or '')
+        js = (self.render_js and
+"""<script type="text/javascript">
     $(document).ready(function() {
-        var newsletter_container = $('#newsletter_container');
+        var newsletter_container = $('#newsletter_container')
         $.get('%s', function(data) {
             newsletter_container.html(data);
         });
@@ -42,7 +48,8 @@ class NewsletterNode(template.Node):
         });
     });
 </script>
-""" % reverse('newsletter:signup_form'))
+""" % reverse('newsletter:signup_form') or '')
+        return (html + js)
 
 @register.tag
 def newsletter(parser, token):
@@ -57,5 +64,26 @@ def newsletter(parser, token):
 
     This is where the ajax responses will place the returned HTML from the view.
 
+    `{% newsletter %}` accepts one optional argument `{% newsletter nojs %}`
+
+    This will only create an empty container while allowing the developer to place the
+    javascript somewhere other than right after the div element.
+
+    This is to fix an issue where some members of the DOM will not allow <script> elements.
+    Also, it is "best practice" to place all <script> tags at the bottom before the </body>
+
     """
-    return NewsletterNode()
+    return NewsletterNode(True, False if token.split_contents().pop() == 'nojs' else True)
+
+@register.tag
+def newsletter_js(parser, token):
+    """
+    {% newsletter_js %}
+
+    Will output the javascript for the newsletter tag
+
+    Probably will be used in conjunction with the `{% newsletter nojs %}`
+    version of the tag
+    """
+    return NewsletterNode(False, True)
+
