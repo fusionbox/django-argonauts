84d79d831dd73a2d70792f78fee956c6454b0860
Chris Proto <cproto@fusionbox.com>
2012-05-30 15:40:44 -0600

    remove add_permission from newsletter admin

--- fusionbox/newsletter/admin.py
+++ fusionbox/newsletter/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import *
+from .models import Submission
+
 
 class NewsletterAdmin(admin.ModelAdmin):
-    readonly_fields = ('email','created_at')
+    readonly_fields = ('email', 'created_at')
+
+    def has_add_permission(self, request, obj=None):
+        return False
 
 admin.site.register(Submission, NewsletterAdmin)
