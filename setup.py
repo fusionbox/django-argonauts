527d8b02a2c0ea59fd17fb4278844b96c9c866ea
Chris Proto <cproto@fusionbox.com>
2012-06-29 12:01:37 -0600

    must use the child directory for package_data

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(name='django-fusionbox',
         'fusionbox.management', 'fusionbox.management.commands'
         ],
     package_data={
-        'fusionbox': ['static/*'],
+        'fusionbox': ['static/js/*'],
         'fusionbox.panels.user_panel': ['templates/*',],
         'fusionbox.newsletter': ['templates/newsletter/*',]
         },
