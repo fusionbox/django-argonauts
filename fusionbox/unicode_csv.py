a2fd1c3c25b8af60136fdd64627bff344704b47f
Chris Proto <cproto@fusionbox.com>
2012-11-01 08:45:10 -0600

    Add support for other encodings aside from utf-8

--- fusionbox/unicode_csv.py
+++ fusionbox/unicode_csv.py
@@ -8,18 +8,20 @@ import codecs
 import cStringIO
 
 
-class UTF8Recoder:
+class UnicodeRecoder:
     """
-    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    Iterator that reads an encoded stream and reencodes the input to the
+    specified encoding
     """
     def __init__(self, f, encoding):
         self.reader = codecs.getreader(encoding)(f)
+        self.encoding = encoding
 
     def __iter__(self):
         return self
 
     def next(self):
-        return self.reader.next().encode("utf-8")
+        return self.reader.next().encode(self.encoding)
 
 
 class UnicodeReader:
@@ -29,14 +31,15 @@ class UnicodeReader:
     """
 
     def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        f = UTF8Recoder(f, encoding)
+        f = UnicodeRecoder(f, encoding)
+        self.encoding = encoding
         self.reader = csv.reader(f, dialect=dialect, **kwds)
         self.line_num = 0
 
     def next(self):
         row = self.reader.next()
         self.line_num = self.reader.line_num
-        return [unicode(s, "utf-8") for s in row]
+        return [unicode(s, self.encoding) for s in row]
 
     def __iter__(self):
         return self
@@ -52,12 +55,13 @@ class UnicodeWriter:
         self.queue = cStringIO.StringIO()
         self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
         self.stream = f
+        self.encoding = encoding
         self.encoder = codecs.getincrementalencoder(encoding)()
 
     def writerow(self, row):
-        self.writer.writerow([unicode(s).encode("utf-8") for s in row])
+        self.writer.writerow([unicode(s).encode(self.encoding) for s in row])
         data = self.queue.getvalue()
-        data = data.decode("utf-8")
+        data = data.decode(self.encoding)
         data = self.encoder.encode(data)
         self.stream.write(data)
         self.queue.truncate(0)
