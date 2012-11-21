d252c4dd111963ea482f6268b8389dc84c423b3d
Gavin Wahl <gwahl@fusionbox.com>
2012-11-21 12:39:35 -0700

    DecoratorMixin
    Converts a view decorator into a View mixin.

--- fusionbox/views/__init__.py
+++ fusionbox/views/__init__.py
@@ -2,3 +2,33 @@
 Fusionbox generic views.
 """
 from .rest import RestView
+
+
+def DecoratorMixin(decorator):
+    """
+    Convers a decorator written for a function view into a mixin for a
+    class-based view.
+
+    ::
+
+        LoginRequiredMixin = DecoratorMixin(login_required)
+
+        class MyView(LoginRequiredMixin):
+            pass
+
+        class SomeView(DecoratorMixin(some_decorator),
+                       DecoratorMixin(something_else)):
+            pass
+
+    """
+
+    class Mixin(object):
+        __doc__ = decorator.__doc__
+
+        @classmethod
+        def as_view(cls, *args, **kwargs):
+            view = super(Mixin, cls).as_view(*args, **kwargs)
+            return decorator(view)
+
+    Mixin.__name__ = 'DecoratorMixin(%s)' % decorator.__name__
+    return Mixin
