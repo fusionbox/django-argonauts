a0f0d0864a4cd3fda413cc0b2310be55ce9e89d0
David Sanders <dsanders@fusionbox.com>
2012-11-27 14:06:00 -0700

    Refactor backupdb command

--- fusionbox/core/management/commands/backupdb.py
+++ fusionbox/core/management/commands/backupdb.py
@@ -2,76 +2,190 @@
 Adapted from http://djangosnippets.org/snippets/823/
 """
 
-import os, time, pipes, popen2
+import os
+import pipes
+from subprocess import Popen, PIPE
+import time
+
 from django.core.management.base import BaseCommand
 
+BACKUP_DIR = 'backup'
+
+
 class Command(BaseCommand):
-    help = "Backs up each database in settings.DATABASES."
+    help = 'Backs up each database in settings.DATABASES.'
     can_import_settings = True
+
+    class BackupError(Exception):
+        pass
+
     def handle(self, *args, **options):
         from django.conf import settings
 
-        backup_dir = 'tmp'
-        if not os.path.exists(backup_dir):
-            os.makedirs(backup_dir)
+        current_time = time.strftime('%F-%s')
 
+        if not os.path.exists(BACKUP_DIR):
+            os.makedirs(BACKUP_DIR)
+
+        # Loop through databases and backup
         for database_name in settings.DATABASES:
             config = settings.DATABASES[database_name]
-            outfile = os.path.join(backup_dir, '%s-%s.sql.gz' % (database_name, time.strftime('%F-%s')))
+
+            # MySQL command and args
             if config['ENGINE'] == 'django.db.backends.mysql':
-                self.do_mysql_backup(outfile,
-                                     config['NAME'],
-                                     config['USER'],
-                                     config.get('PASSWORD', None),
-                                     config.get('HOST', None),
-                                     config.get('PORT', None))
+                backup_cmd = self.do_mysql_backup
+                backup_kwargs = {
+                    'timestamp_file': os.path.join(BACKUP_DIR, '{0}-{1}.mysql.gz'.format(database_name, current_time)),
+                    'latest_file': os.path.join(BACKUP_DIR, '{0}-latest.mysql.gz'.format(database_name)),
+                    'db': config['NAME'],
+                    'user': config['USER'],
+                    'password': config.get('PASSWORD', None),
+                    'host': config.get('HOST', None),
+                    'port': config.get('PORT', None),
+                }
+            # PostgreSQL command and args
             elif config['ENGINE'] == 'django.db.backends.postgresql_psycopg2':
-                self.do_postgresql_backup(outfile,
-                                     config['NAME'],
-                                     config['USER'],
-                                     config.get('PASSWORD', None),
-                                     config.get('HOST', None),
-                                     config.get('PORT', None))
+                backup_cmd = self.do_postgresql_backup
+                backup_kwargs = {
+                    'timestamp_file': os.path.join(BACKUP_DIR, '{0}-{1}.pgsql.gz'.format(database_name, current_time)),
+                    'latest_file': os.path.join(BACKUP_DIR, '{0}-latest.pgsql.gz'.format(database_name)),
+                    'db': config['NAME'],
+                    'user': config['USER'],
+                    'password': config.get('PASSWORD', None),
+                    'host': config.get('HOST', None),
+                    'port': config.get('PORT', None),
+                }
+            # SQLite command and args
+            elif config['ENGINE'] == 'django.db.backends.sqlite3':
+                backup_cmd = self.do_sqlite_backup
+                backup_kwargs = {
+                    'timestamp_file': os.path.join(BACKUP_DIR, '{0}-{1}.sqlite.gz'.format(database_name, current_time)),
+                    'latest_file': os.path.join(BACKUP_DIR, '{0}-latest.sqlite.gz'.format(database_name)),
+                    'db_file': config['NAME'],
+                }
+            # Unsupported
             else:
-                print 'Backup in %s engine not implemented' % config['ENGINE']
+                backup_cmd = None
 
+            # Run backup command with args
+            print '========== Backing up \'{0}\'...'.format(database_name)
+            if backup_cmd:
+                try:
+                    backup_cmd(**backup_kwargs)
+                    print '========== ...done!'
+                except self.BackupError as e:
+                    print e.message
+                    print '========== ...skipped.'
+            else:
+                print 'Backup for {0} engine not implemented.'.format(config['ENGINE'])
+                print '========== ...skipped.'
+            print ''
 
-    def do_mysql_backup(self, outfile, db, user, password=None, host=None, port=None):
-        args = []
-        if user:
-            args += ["--user=%s" % pipes.quote(user)]
+    def do_mysql_backup(self, timestamp_file, latest_file, db, user, password=None, host=None, port=None):
+        # Build args to dump command
+        dump_args = []
+        dump_args += ['--user={0}'.format(pipes.quote(user))]
         if password:
-            args += ["--password=%s" % pipes.quote(password)]
+            dump_args += ['--password={0}'.format(pipes.quote(password))]
         if host:
-            args += ["--host=%s" % pipes.quote(host)]
+            dump_args += ['--host={0}'.format(pipes.quote(host))]
         if port:
-            args += ["--port=%s" % pipes.quote(port)]
-        args += [pipes.quote(db)]
+            dump_args += ['--port={0}'.format(pipes.quote(port))]
+        dump_args += [pipes.quote(db)]
+        dump_args = ' '.join(dump_args)
 
-        c = 'mysqldump %s | gzip > %s' % (' '.join(args), outfile)
-        print c
-        os.system(c)
-        print "Backed up %s; Load with `cat %s | gunzip | mysql %s`" % (db, pipes.quote(outfile), ' '.join(args))
+        # Build filenames
+        timestamp_file = pipes.quote(timestamp_file)
+        latest_file = pipes.quote(latest_file)
 
+        # Build command
+        cmd = 'mysqldump {dump_args} | gzip > {timestamp_file} && cp {timestamp_file} {latest_file}'.format(
+            dump_args=dump_args,
+            timestamp_file=timestamp_file,
+            latest_file=latest_file,
+        )
 
-    def do_postgresql_backup(self, outfile, db, user, password=None, host=None, port=None):
-        args = []
-        if user:
-            args += ["--username=%s" % user]
+        # Execute
+        self.do_command(cmd, db)
+
+        print 'Backed up {db}; Load with `cat {timestamp_file} | gunzip | mysql {dump_args}`'.format(
+            db=db,
+            timestamp_file=timestamp_file,
+            dump_args=dump_args,
+        )
+
+    def do_postgresql_backup(self, timestamp_file, latest_file, db, user, password=None, host=None, port=None):
+        # Build args to dump command
+        dump_args = []
+        dump_args += ['--username={0}'.format(pipes.quote(user))]
         if password:
-            args += ["--password"]
+            dump_args += ['--password']
         if host:
-            args += ["--host=%s" % host]
+            dump_args += ['--host={0}'.format(pipes.quote(host))]
         if port:
-            args += ["--port=%s" % port]
-        if db:
-            args += [db]
-        c = 'pg_dump %s | gzip > %s' % (' '.join(args), outfile)
-        print c
-        pipe = popen2.Popen4(c)
-        if password:
-            pipe.tochild.write('%s\n' % password)
-            pipe.tochild.close()
-        retcode = pipe.wait()
-        assert retcode == 0
-        print "Backed up %s; Load with `cat %s | gunzip | psql %s`" % (db, pipes.quote(outfile), ' '.join(args))
+            dump_args += ['--port={0}'.format(pipes.quote(port))]
+        dump_args += [pipes.quote(db)]
+        dump_args = ' '.join(dump_args)
+
+        # Build filenames
+        timestamp_file = pipes.quote(timestamp_file)
+        latest_file = pipes.quote(latest_file)
+
+        # Build command
+        cmd = 'pg_dump {dump_args} | gzip > {timestamp_file} && cp {timestamp_file} {latest_file}'.format(
+            dump_args=dump_args,
+            timestamp_file=timestamp_file,
+            latest_file=latest_file,
+        )
+
+        # Execute
+        self.do_command(cmd, db, password)
+
+        print 'Backed up {db}; Load with `cat {timestamp_file} | gunzip | psql {dump_args}`'.format(
+            db=db,
+            timestamp_file=timestamp_file,
+            dump_args=dump_args,
+        )
+
+    def do_sqlite_backup(self, timestamp_file, latest_file, db_file):
+        # Build filenames
+        db_file = pipes.quote(db_file)
+        timestamp_file = pipes.quote(timestamp_file)
+        latest_file = pipes.quote(latest_file)
+
+        # Build command
+        cmd = 'gzip < {db_file} > {timestamp_file} && cp {timestamp_file} {latest_file}'.format(
+            db_file=db_file,
+            timestamp_file=timestamp_file,
+            latest_file=latest_file,
+        )
+
+        # Execute
+        self.do_command(cmd, db_file)
+
+        print 'Backed up {db_file}; Load with `cat {timestamp_file} | gunzip > {db_file}`'.format(
+            db_file=db_file,
+            timestamp_file=timestamp_file,
+        )
+
+    def do_command(cls, cmd, db, password=None):
+        """
+        Executes a command and prints a status message.
+        """
+        print 'executing:'
+        print cmd
+
+        with open('/dev/null', 'w') as FNULL:
+            process = Popen(cmd, stdin=PIPE, stdout=FNULL, stderr=FNULL, shell=True)
+
+            # Enter a password through stdin if required
+            if password:
+                process.communicate(input='{0}\n'.format(password))
+            else:
+                process.wait()
+
+            if process.returncode != 0:
+                raise cls.BackupError('Error code {code} while backing up database \'{db}\'!'.format(
+                    code=process.returncode,
+                    db=db,
+                ))
