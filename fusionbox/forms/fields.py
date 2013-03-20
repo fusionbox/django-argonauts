8cfac3bd96f15458541b3b35fe2d28dafa10e5a4
Gavin Wahl <gwahl@fusionbox.com>
2013-03-20 16:05:04 -0600

    Add allow_extension to PhoneNumberField
    Set allow_extension to False to disallow phone numbers with extensions.

--- fusionbox/forms/fields.py
+++ fusionbox/forms/fields.py
@@ -149,15 +149,22 @@ class BetterReadOnlyPasswordHashWidget(ReadOnlyPasswordHashWidget):
 
 class PhoneNumberField(forms.CharField):
     """
-    A USA or international phone number field. Normalizes its value to a common
-    US format '(303) 555-5555' for US phone numbers, and an international
-    format for others - '+86 10 6944 5464'. Also supports extensions --
-    '3035555555ex12' -> '(303) 555-5555 ext. 12.'
+    A USA or international phone number field. Normalizes its value to
+    a common US format '(303) 555-5555' for US phone numbers, and an
+    international format for others -- '+86 10 6944 5464'. Also supports
+    extensions -- '3035555555ex12' -> '(303) 555-5555 ext. 12.'
+
+    If you do not wish to allow phone numbers with extensions, use
+    `allow_extension=False`.
     """
 
     default_error_messages = {
         'invalid': 'Enter a valid phone number.',
+        'extension': 'A phone number with an extension is not supported.',
     }
+
+    def __init__(self, *args, **kwargs):
+        self.allow_extension = kwargs.pop('allow_extension', True)
 
     def clean(self, value):
         if not value:
@@ -168,6 +175,8 @@ class PhoneNumberField(forms.CharField):
             raise forms.ValidationError(self.error_messages['invalid'])
         if not phonenumbers.is_valid_number(number):
             raise forms.ValidationError(self.error_messages['invalid'])
+        if not self.allow_extension and number.extension is not None:
+            raise forms.ValidationError(self.error_messages['extension'])
 
         if number.country_code == 1:
             return phonenumbers.format_number(number, phonenumbers.PhoneNumberFormat.NATIONAL)
