ce1ec89ab7a95b5f02f69819e88fb94fb976cc73
Gavin Wahl <gwahl@fusionbox.com>
2013-04-23 18:36:34 -0600

    just use auto_now_add for created_at

--- fusionbox/behaviors.py
+++ fusionbox/behaviors.py
@@ -280,7 +280,7 @@ class Timestampable(Behavior):
     class Meta:
         abstract = True
 
-    created_at = models.DateTimeField(default=now, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
