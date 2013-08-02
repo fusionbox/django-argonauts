83d7bb9526d6d5ff22aabe91bc92bffa4f69df6b
Rocky Meza <rocky@fusionbox.com>
2013-08-02 15:41:15 -0600

    Fix setup.py
    - Fusionbox name and email
    - install requires
    - packages list
    - also fixed the name in LICENSE

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ version = __import__('argonauts').get_version()
 
 setup(name='django-argonauts',
       version=version,
-      author="Fusionbox Inc",
-      author_email="fusionbox@fusionbox.com",
+      author="Fusionbox, Inc.",
+      author_email="programmers@fusionbox.com",
       keywords="rest json views django helpers",
       classifiers=[
           'Development Status :: 4 - Beta',
@@ -13,9 +13,17 @@ setup(name='django-argonauts',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: BSD License',
           'Topic :: Internet :: WWW/HTTP',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
       ],
-
-      packages=['argonauts'],
+      install_requires=['Django>=1.4'],
+      packages=[
+          'argonauts',
+          'argonauts.templatetags',
+      ],
 
       test_suite='testproject.runtests',
 )
