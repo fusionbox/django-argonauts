4122c7ff0d7f2e2e09200aaf410e47e136a37935
Rocky Meza <rocky@fusionbox.com>
2013-08-02 16:10:25 -0600

    Added tests for the |json filter.

--- argonauts/tests.py
+++ argonauts/tests.py
@@ -2,7 +2,14 @@ import json
 import datetime
 import decimal
 
-from django.utils import unittest
+try:
+    from django.utils import unittest
+except ImportError:  # Django >= 1.7
+    import unittest
+
+from django.template import Template, Context
+from django.test.utils import override_settings
+from django.utils.datastructures import SortedDict
 
 from argonauts import dumps
 
@@ -40,3 +47,35 @@ class TestJson(unittest.TestCase):
         except ImportError:
             from django.contrib.auth.models import User  # NOQA
         self.assertion(User.objects.filter(id=None), [])
+
+
+class TestJsonTemplateFilter(unittest.TestCase):
+    template = "{% load argonauts %}{{ data|json }}"
+
+    def render_data(self, data):
+        template = Template(self.template)
+        return template.render(Context({'data': data}))
+
+    def render_dictionary(self):
+        return self.render_data(SortedDict([
+            ('a', 'foo'),
+            ('b', TestObject('bar')),
+        ]))
+
+    def test_json_escapes_unsafe_characters(self):
+        rendered = self.render_data("<script>alert('&XSS!');</script>")
+
+        self.assertEqual(rendered, '"\\u003cscript\\u003ealert(\'\\u0026XSS!\');\\u003c/script\\u003e"')
+
+    @override_settings(DEBUG=True)
+    def test_pretty_rendering_in_debug(self):
+        rendered = self.render_dictionary()
+        self.assertEqual(rendered, """{
+    "a": "foo",
+    "b": "bar"
+}""")
+
+    @override_settings(DEBUG=False)
+    def test_compact_rendering_no_debug(self):
+        rendered = self.render_dictionary()
+        self.assertEqual(rendered, '{"a":"foo","b":"bar"}')
