18c37dd66e42af67d200d84f136b68aeb725e31a
Rocky Meza <rocky@fusionbox.com>
2014-08-21 15:45:39 -0600

    Use force_text for lazy_string serialization.

--- argonauts/serializers.py
+++ argonauts/serializers.py
@@ -2,6 +2,11 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models.query import QuerySet
 from django.utils.functional import Promise
 
+try:
+    from django.utils.encoding import force_text
+except ImportError:  # Django < 1.4
+    from django.utils.encoding import force_unicode as force_text  # NOQA
+
 
 class JSONArgonautsEncoder(DjangoJSONEncoder):
     """
@@ -14,13 +19,8 @@ class JSONArgonautsEncoder(DjangoJSONEncoder):
         if isinstance(o, QuerySet):
             return list(o)
 
-        if isinstance(o, Promise) and hasattr(o, 'ljust'):
-            # Force lazy string evaluation to prevent errors:
-            # - not serializable as JSON without changes
-            # - circular reference when converting into text using six.u()
-            # While not clean, this seemed a safe method that does not assume
-            # whether it's text or binary.
-            # FIXME: find a cleaner solution and check if this is a Django bug
-            return o.ljust(0)
+        if isinstance(o, Promise):
+            # Serialize lazy translated strings
+            return force_text(o)
 
         return super(JSONArgonautsEncoder, self).default(o)
