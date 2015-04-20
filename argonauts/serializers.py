669fb7fc66771e7505716bb5a1844b86145cdc65
Antoine Catton <acatton@fusionbox.com>
2015-04-20 17:22:52 -0600

    force_unicode is deprecated

--- argonauts/serializers.py
+++ argonauts/serializers.py
@@ -1,7 +1,12 @@
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models.query import QuerySet
 from django.utils.functional import Promise
-from django.utils.encoding import force_unicode
+
+try:
+    from django.utils.encoding import force_text
+except ImportError:
+    # BBB: Django 1.4
+    from django.utils.encoding import force_unicode as force_text
 
 
 class JSONArgonautsEncoder(DjangoJSONEncoder):
@@ -16,6 +21,6 @@ class JSONArgonautsEncoder(DjangoJSONEncoder):
             return list(o)
 
         if isinstance(o, Promise):
-            return force_unicode(o)
+            return force_text(o)
 
         return super(JSONArgonautsEncoder, self).default(o)
